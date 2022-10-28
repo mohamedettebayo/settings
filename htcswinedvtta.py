cb5a73106e679c5a2dcf34b4b8d0259696419b56
mohamedettebayo <mohamedettebayo@gmail.com>
2022-10-28 11:33:53 +0300

    Update htcswinedvtta.py

--- htcswinedvtta.py
+++ htcswinedvtta.py
@@ -1,6 +1,6 @@
 model = dict(
     type='HybridTaskCascade',
-    pretrained=None,
+    pretrained="/kaggle/working/mmdetection/htc_cbv2_swin_large22k_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_1x_coco.pth",
     backbone=dict(
         type='CBSwinTransformer',
         embed_dim=192,
@@ -428,7 +428,7 @@ log_config = dict(
 custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = None
+load_from = "/kaggle/working/mmdetection/htc_cbv2_swin_large22k_patch4_window7_mstrain_400-1400_giou_4conv1f_adamw_1x_coco.pth"
 resume_from = None
 workflow = [('train', 1)]
 samples_per_gpu = 1
